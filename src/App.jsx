b70893d1544f955c7c05e6ae8f20e219bb42d33b
bikash sarker <bikash.webdeveloper5@gmail.com>
2024-11-03 21:17:44 +0600

    the project setup ready

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  );
+}
+
+export default App;
