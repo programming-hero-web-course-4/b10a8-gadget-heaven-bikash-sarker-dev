085064b0da6b57277979ed6e3b55ad6dd2fde30b
bikash sarker <bikash.webdeveloper5@gmail.com>
2024-11-05 21:34:51 +0600

    wish list add to button add condiational

--- src/components/dashboard/CardDashboard.jsx
+++ src/components/dashboard/CardDashboard.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import { useContext } from "react";
+import { FiShoppingCart } from "react-icons/fi";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import { DashboardContext } from "../../layout/Root";
 
 const CardDashboard = ({ card, handleRemoveCard }) => {
+  const { dashCardWish } = useContext(DashboardContext);
   const { product_image, product_title, price, description } = card;
 
   return (
     <div className="flex gap-5 items-center bg-t-primary rounded-2xl p-6 my-6">
       <div className="w-1/5">
         <img
-          className="w-full rounded-xl h-[150px] object-cover"
+          className={`w-full rounded-xl object-cover ${
+            dashCardWish ? "h-[150px] " : "h-[200px]"
+          } `}
           src={product_image}
           alt=""
         />
@@ -18,6 +23,18 @@ const CardDashboard = ({ card, handleRemoveCard }) => {
           <h3 className="text-2xl font-semibold ">{product_title}</h3>
           <p className="opacity-60 mt-5 mb-4">{description}</p>
           <p className="font-medium opacity-60 mt-2 mb-5">Price: $ {price}</p>
+          {dashCardWish ? (
+            " "
+          ) : (
+            <div>
+              <button
+                //   onClick={() => handleAddCard(detailsFind)}
+                className="btn bg-p-primary rounded-3xl px-6 text-t-primary hover:text-p-primary"
+              >
+                Add To Card <FiShoppingCart className="text-xl" />
+              </button>
+            </div>
+          )}
         </div>
         <div>
           <button
